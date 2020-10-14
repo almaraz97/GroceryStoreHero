f464b8caadca143c610405341eada48ec87b3fe6
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2020-10-14 18:41:37 -0400

    Add files via upload

---
+++ GroceryHero/Recipes/forms.py
@@ -0,0 +1,140 @@
+from flask_wtf import FlaskForm
+from werkzeug.routing import ValidationError
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, FloatField, FieldList, FormField
+from wtforms.validators import DataRequired
+
+
+class Measurements:
+    Measures = ['Unit', 'Package', 'Can', 'Bottle', 'Jar', 'US Cup', 'US Tablespoon', 'US Teaspoon', 'US Fluid Ounce',
+                'Ounce', 'Pound', 'Milligram', 'Gram', 'Kilogram', 'Milliliter', 'Liter']
+    # 'US Pint', 'US Quart', 'US Gallon',
+
+    Volumes = ['US Cup', 'US Fluid Ounce', 'US Tablespoon', 'US Teaspoon']  # 'US Gallon', 'US Quart', 'US Pint',
+    Weights = ['Pound', 'Ounce']
+    Generic = ['Unit', 'Package', 'Can', 'Bottle', 'Jar']
+
+    Metric_Volumes = ['Liter', 'Milliliter']
+    Metric_Weights = ['Kilogram', 'Gram', 'Milligram']
+
+    Convert = {'US Gallon': 768, 'US Quart': 192, 'US Pint': 96, 'US Cup': 48.6922, 'US Fluid Ounce': 6,
+               'US Tablespoon': 3, 'US Teaspoon': 1, 'Liter': 1000, 'Milliliter': 1, 'Pound': 16, 'Ounce': 1,
+               'Kilogram': 1e+6, 'Gram': 1000, 'Milligram': 1}  # To lowest [teaspoon, ounce, milligram]
+
+    # Dict_func = (lambda V=Volumes, W=Weights, G=Generic, MV=Metric_Volumes, M=Measures:
+    #              {x: ('Volume' if x in V else
+    #                   ('Weight' if x in W else
+    #                    ('Generic' if x in G else
+    #                     ('Metric_Volumes' if x in MV else 'Metric_Weights')))) for x in M})
+    # Measure_dict = Dict_func()
+
+    def __init__(self, value, unit):
+        if unit not in self.Measures:
+            raise AssertionError("Must be in Measurements")
+        self.metric = True if unit in ['Milligram', 'Gram', 'Kilogram', 'Liter', 'Milliliter'] else False
+        self.value = value
+        self.unit = unit
+        if self.unit in self.Generic:
+            self.type = 'Generic'
+        else:
+            if self.metric:  # Should I do both volumes/weight or Volume_metric/Weight_metric
+                self.type = 'Volume' if self.unit in self.Metric_Volumes else 'Weight'
+            else:
+                self.type = 'Volume' if self.unit in self.Volumes else 'Weight'
+
+    def __add__(self, other, rounding=2):
+        if not isinstance(other, Measurements):
+            raise AssertionError('Must be of class Measurements')
+        if not self.metric == other.metric:
+            raise AssertionError('Must agree in measurement system')
+        if self.type != other.type:
+            AssertionError('Cannot add objects of different measure types')
+        if self.type == 'Generic':
+            if self.unit == other.unit:
+                total = self.value + other.value
+                return Measurements(round(total, rounding), unit=self.unit)
+            else:
+                raise AssertionError('Must agree in Generic unit')
+
+        self.value = self.Convert[self.unit] * self.value  # converted to lowest
+        other.value = other.Convert[other.unit] * other.value  # converted to lowest
+        total = self.value + other.value
+        if self.type == 'Volume':
+            volumes = self.Metric_Volumes if self.metric else self.Volumes
+            for volume in volumes:  # Go up through conversion until whole number
+                if int(total / self.Convert[volume]) >= 1:
+                    total = total / self.Convert[volume]
+                    return Measurements(round(total, 2), unit=volume)
+        else:
+            weights = self.Metric_Weights if self.metric else self.Weights
+            for weight in weights:
+                if int(total / self.Convert[weight]) >= 1:
+                    total = total / self.Convert[weight]
+                    return Measurements(round(total, 2), unit=weight)
+
+    def __repr__(self):
+        if self.value != 1:
+            return f'{self.value} {self.unit}s'
+        else:
+            return f'{self.value} {self.unit}'
+
+
+# class MyFloatField(FloatField):
+#     def process_data(self, valuelist):
+#         if valuelist:
+#             if valuelist.count('/') == 1:
+#                 try:
+#                     numbers = valuelist.split('/')
+#                     self.data = numbers[0]/numbers[1]
+#                 except ValueError:
+#                     self.data = None
+#                     raise ValueError(self.gettext('Not a valid division'))
+#             else:
+#                 try:
+#                     self.data = valuelist[0]
+#                 except ValueError:
+#                     self.data = None
+#                     raise ValueError(self.gettext('Not a valid float value'))
+
+
+class RecipeForm(FlaskForm):
+    title = StringField('Recipe Name', validators=[DataRequired()])
+    content = TextAreaField('Ingredients (separate with commas)', validators=[DataRequired()])
+    notes = TextAreaField('Notes (optional)')
+    submit = SubmitField('Next')
+
+
+def Frac_Number_Validator(form, field):
+    try:
+        float(field.data)
+    except ValueError:
+        if field.data.count('/') == 1 and ''.join(i for i in field.data if i not in ['/', ' ']).isnumeric():
+            pass
+        else:
+            raise ValidationError('Enter a number or a fraction')
+
+    if field.data.count('/') == 1 and ''.join(i for i in field.data if i not in ['/', ' ']).isnumeric():  # div and num
+        num = field.data.split('/')
+        return float(num[0]) / float(num[1])
+    elif field.data.isnumeric():
+        float(field.data)
+    elif field.data.count('/') != 1:  #
+        raise ValidationError('Not')
+    else:
+        try:
+            return float(field.data)
+        except ValueError:
+            raise ValidationError('')
+
+
+class QuantityFormNoCSFR(FlaskForm):
+    # class Meta:
+    #     csrf = False
+    ingredient_quantity = StringField('Quantity', default=1.0, validators=[DataRequired()])
+    ingredient_type = SelectField("Measurement", choices=Measurements.Measures, default='Unit')
+
+
+class FullQuantityForm(FlaskForm):
+    ingredients = []
+    ingredient_forms = FieldList(FormField(QuantityFormNoCSFR), min_entries=1)
+    notes = ''
+    submit = SubmitField('Submit')
---
+++ GroceryHero/Recipes/routes.py
@@ -0,0 +1,300 @@
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from GroceryHero.Main.utils import update_grocery_list, ensure_harmony_keys, get_harmony_settings
+from GroceryHero.Users.forms import HarmonyForm
+from GroceryHero.Recipes.forms import RecipeForm, FullQuantityForm
+from GroceryHero.HarmonyTool import recipe_stack
+from GroceryHero.models import Recipes
+from GroceryHero import db
+import string
+import json
+
+recipes = Blueprint('recipes', __name__)
+
+
+@recipes.route('/recipes', methods=['GET', 'POST'])
+def recipes_page(possible=0, recommended=None):
+    if current_user.is_authenticated:
+        recipe_list = Recipes.query.filter_by(author=current_user).order_by(Recipes.title).all()  # Get all recipes
+        in_menu = [recipe for recipe in recipe_list if recipe.in_menu]  # Recipe objects that are in menu
+        for i, recipe in enumerate(in_menu):  # Puts menu items first in recipe_list
+            recipe_list.remove(recipe)
+            recipe_list.insert(i, recipe)
+        in_menu = [recipe.title for recipe in in_menu]  # List of recipe titles in menu
+        recipe_history = [item for sublist in current_user.history[:current_user.harmony_preferences['history']]
+                          for item in sublist]
+        recipe_history = [x.title for x in Recipes.query.filter(Recipes.id.in_(recipe_history)).all()]
+        form = HarmonyForm()  # Recipe Harmony Tool Form
+        form.groups.choices = [x for x in range(2 - len(in_menu), 5) if 0 < x]
+        excludes = [recipe.title for recipe in recipe_list if recipe.title not in (in_menu + recipe_history)]
+        form.excludes.choices = [x for x in zip([0] + excludes, ['-- select options (clt+click) --'] + excludes)]
+
+        if request.method == 'GET':
+            check_preferences(current_user)
+            preferences = current_user.harmony_preferences  # Load user's previous preferences dictionary
+            form.similarity.data = preferences['similarity']
+            form.groups.data = preferences['groups']
+            possible = preferences['possible']
+            if preferences['recommended'] and preferences['recommended'] != '':  # If saved recommended is not empty
+                recommended = {tuple(group.split(', ')): preferences['recommended'][group] for
+                               group in preferences['recommended']}
+
+        elif request.method == 'POST':  # Harmony or search button was pressed  # todo only on form submit
+            preferences = get_harmony_settings(current_user.harmony_preferences)
+            recipes = {r.title: r.quantity.keys() for r in recipe_list}
+            count = int(form.groups.data)  # + len(in_menu) if form.groups.data else len(in_menu)
+            recommended, possible = recipe_stack(recipes, count, max_sim=form.similarity.data,
+                                                 excludes=form.excludes.data + recipe_history, includes=in_menu,
+                                                 limit=500_000, **preferences)
+            in_menu = None if len(in_menu) < 1 else in_menu
+            if in_menu is not None:
+                for group in list(recommended.keys()):
+                    recommended[tuple([x for x in group if x not in in_menu])] = recommended[group]
+                    del recommended[group]
+            # Update user preferences
+            preference = {key: current_user.harmony_preferences[key] for key in current_user.harmony_preferences}
+            preference['similarity'] = form.similarity.data
+            preference['groups'] = form.groups.data
+            preference['recommended'] = {', '.join(list(group)): recommended[group] for group in recommended}
+            preference['possible'] = possible
+            current_user.harmony_preferences = preference
+            db.session.commit()
+        return render_template('recipes.html', title='Recipes', cards=recipe_list,
+                               recipe_ids={recipe.title: recipe.id for recipe in recipe_list},
+                               search_recipes=sorted(recipe_list, key=lambda x: x.title),
+                               sidebar=True, combos=possible, recommended=recommended, form=form)
+    return render_template('recipes.html', recipes=None, all_recipes=None, title='Recipes', sidebar=False, combos=0,
+                           recommended=None)
+
+
+@recipes.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_recipe():  # todo could use session to transfer recipe to quantity page
+    if len(Recipes.query.filter_by(author=current_user).all()) > 75:  # User recipe limit
+        return redirect(url_for('main.account'))
+    form = RecipeForm()
+    if form.validate_on_submit():  # Send data to quantity page
+        ingredients = [string.capwords(x.strip()) for x in form.content.data.split(',') if x.strip() != '']
+        quantity = {ingredient: [1, 'Unit'] for ingredient in ingredients}
+        notes = form.notes.data.replace('/', '\\')  # This gets sent through to the next route
+        recipe = json.dumps({'title': form.title.data, 'quantity': quantity, 'notes': notes})
+        return redirect(url_for('recipes.new_recipe_quantity', recipe=recipe))
+    return render_template('create_recipe.html', title='New Recipe', form=form, legend='New Recipe')
+
+
+@recipes.route('/post/new/quantity/<string:recipe>', methods=['GET', 'POST'])
+@login_required
+def new_recipe_quantity(recipe):
+    recipe = json.loads(recipe)  # Has {RecipeName: string, Quantity: {ingredient: [value,type]}}
+    data = {'ingredient_forms': [{'ingredient_quantity': recipe['quantity'][ingredient][0],
+                                  'ingredient_type': recipe['quantity'][ingredient][1]}
+                                 for ingredient in recipe['quantity'].keys()]}
+    form = FullQuantityForm(data=data)
+    form.ingredients = [x for x in recipe['quantity'].keys()]
+    if form.is_submitted():
+        quantity = [data['ingredient_quantity'] for data in form.ingredient_forms.data]
+        measure = [data['ingredient_type'] for data in form.ingredient_forms.data]
+        formatted = {ingredient: [Q, M] for ingredient, Q, M in zip(form.ingredients, quantity, measure)}
+        recipe = Recipes(title=(recipe['title']).capitalize(), quantity=formatted, author=current_user,
+                         notes=recipe['notes'])
+        db.session.add(recipe)
+        db.session.commit()
+        flash('Your recipe has been created!', 'success')
+        return redirect(url_for('recipes.recipes_page'))
+    return render_template('recipe_quantity.html', title='New Recipe', form=form, legend='Recipe Quantities',
+                           recipe=recipe)
+
+
+@recipes.route('/post/<int:recipe_id>')
+@login_required
+def recipe_single(recipe_id):
+    recipe_post = Recipes.query.get_or_404(recipe_id)
+    if recipe_post.author != current_user:
+        abort(403)
+    quantity = {ingredient: [int(recipe_post.quantity[ingredient][0]),
+                             recipe_post.quantity[ingredient][1]] if
+    float(recipe_post.quantity[ingredient][0]).is_integer() else
+    [recipe_post.quantity[ingredient][0], recipe_post.quantity[ingredient][1]]
+                for ingredient in recipe_post.quantity.keys()}
+    recipe_post.quantity = quantity
+    return render_template('recipe.html', title=recipe_post.title, recipe=recipe_post)
+
+
+@recipes.route('/post/<int:recipe_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_recipe(recipe_id):
+    recipe = Recipes.query.get_or_404(recipe_id)
+    if recipe.author != current_user:  # You can only update your own recipes
+        abort(403)
+    form = RecipeForm()
+    if form.validate_on_submit():
+        ingredients = [string.capwords(x.strip()) for x in form.content.data.split(',')]
+        quantity = {ingredient: recipe.quantity[ingredient] if ingredient in recipe.quantity else [1, 'Unit']
+                    for ingredient in ingredients}
+        notes = form.notes.data.replace('/', '\\')
+        title = string.capwords(form.title.data.strip())
+        recipe = json.dumps({'title': title, 'quantity': quantity, 'notes': notes})
+        return redirect(url_for('recipes.update_recipe_quantity', recipe_id=recipe_id, recipe=recipe))
+    elif request.method == 'GET':
+        form.title.data = recipe.title
+        form.content.data = ', '.join(recipe.quantity.keys())
+        form.notes.data = recipe.notes
+    return render_template('create_recipe.html', title='Update Recipe', form=form, legend='Update Recipe')  # todo
+
+
+@recipes.route('/post/<int:recipe_id>/update_quantity/<recipe>', methods=['GET', 'POST'])
+@login_required
+def update_recipe_quantity(recipe_id, recipe):
+    recipe = json.loads(recipe)  # Has {RecipeName: string, Quantity: {ingredient: [value,type]}}
+    data = {'ingredient_forms': [{'ingredient_quantity': recipe['quantity'][ingredient][0],
+                                  'ingredient_type': recipe['quantity'][ingredient][1]}
+                                 for ingredient in recipe['quantity'].keys()]}
+    form = FullQuantityForm(data=data)  # List of dictionaries
+    form.ingredients = [x for x in recipe['quantity'].keys()]
+    if form.is_submitted():
+        title = recipe['title']
+        notes = recipe['notes'].replace('\\', '/')
+        quantity = [pair['ingredient_quantity'] for pair in form.ingredient_forms.data]
+        measure = [data['ingredient_type'] for data in form.ingredient_forms.data]
+        formatted = {ingredient: [Q, M] for ingredient, Q, M in zip(form.ingredients, quantity, measure)}
+        # Load previous data to update
+        recipe = Recipes.query.get_or_404(recipe_id)
+        recipe.title = title
+        recipe.quantity = formatted
+        recipe.notes = notes
+        update_grocery_list(current_user)
+        db.session.commit()
+        flash('Your recipe has been updated!', 'success')
+        return redirect(url_for('recipes.recipe_single', recipe_id=recipe.id))
+    return render_template('recipe_quantity.html', title='Update Recipe', form=form, legend='Recipe Quantities',
+                           recipe=recipe)
+
+
+@recipes.route('/post/<int:recipe_id>/delete', methods=['POST'])
+@login_required
+def delete_recipe(recipe_id):
+    recipe = Recipes.query.get_or_404(recipe_id)
+    if recipe.author != current_user:  # You can only change your own recipes
+        abort(403)
+    db.session.delete(recipe)
+    if recipe.title in current_user.harmony_preferences['recommended']:  # If delete recipe in recommended
+        temp = {key: value for key, value in current_user.harmony_preferences.items()}
+        temp['recommended'] = {}  # Reset recipe tool recommendations
+        temp['possible'] = 0
+        current_user.harmony_preferences = temp
+    update_grocery_list(current_user)
+    db.session.commit()
+    flash('Your recipe has been deleted!', 'success')
+    return redirect(url_for('recipes.recipes_page'))
+
+
+@recipes.route('/recipes/change_menu', methods=['POST'])
+@login_required
+def change_to_menu():  # JavaScript way of adding to menu without reload
+    recipe_id = request.form['recipe_id']
+    recipe = Recipes.query.get_or_404(recipe_id)
+    if recipe.author != current_user:  # You can only update your own recipes # Might not be needed
+        abort(403)
+    recipe.in_menu = not recipe.in_menu
+    update_grocery_list(current_user)
+    db.session.commit()
+    return json.dumps({'result': 'success'})
+
+
+@recipes.route('/recipes/<int:recipe_id>/add_menu', methods=['GET', 'POST'])  # ??REQUIRES 'GET'
+@login_required
+def add_to_menu(recipe_id):  # Adding from RHT recommendations
+    recipe = Recipes.query.get_or_404(recipe_id)
+    if recipe.author != current_user:  # You can only update your own recipes # Might not be needed
+        abort(403)
+    recipe.in_menu = True
+    update_grocery_list(current_user)
+    db.session.commit()
+    return redirect(url_for('recipes.recipes_page'))
+
+
+@recipes.route('/recipes/add_menu', methods=['GET', 'POST'])  # Trying to get from recipe one-more
+@login_required
+def multi_add_to_menu():
+    for recipe_id in request.form.getlist('harmony'):
+        recipe = Recipes.query.get_or_404(recipe_id)
+        if recipe.author != current_user:  # You can only update your own recipes # Might not be needed
+            abort(403)
+        recipe.in_menu = True
+    update_grocery_list(current_user)
+    db.session.commit()
+    return redirect(url_for('recipes.recipes_page'))
+
+
+@recipes.route('/post/search', methods=['GET', 'POST'])
+@login_required
+def recipes_search(recommended=None, possible=0):
+    if current_user.is_authenticated:
+        search = request.form['search']
+        if search == 'Recipe Options' or search == '':
+            return redirect(url_for('recipes.recipes_page'))
+        recipe_list = Recipes.query.filter_by(author=current_user).order_by(Recipes.title).all()  # Get all recipes
+        all_rec = sorted(recipe_list, key=lambda x: x.title)
+        ids = {recipe.title: recipe.id for recipe in recipe_list}
+        cards = [recipe for recipe in recipe_list if search.lower() in recipe.title.lower()]
+        # Sidebar form
+        form = HarmonyForm()
+        choices = [recipe.title for recipe in recipe_list if not recipe.in_menu]  # Can't exclude menu items
+        form.excludes.choices = [x for x in zip([0] + choices, ['-- select options (clt+click) --'] + choices)]
+        if request.method == 'POST':  # Load previous preferences/recommendations
+            preference = current_user.harmony_preferences  # Preference dictionary
+            form.similarity.data = preference['similarity']
+            form.groups.data = preference['groups']
+            possible = preference['possible']
+            if preference['recommended']:  # If saved recommended is not empty
+                recommended = {tuple(group.split(', ')): preference['recommended'][group] for
+                               group in preference['recommended']}
+        return render_template('recipes.html', title='Recipes', cards=cards, recipe_ids=ids, search_recipes=all_rec,
+                               sidebar=True, combos=possible, recommended=recommended, form=form)
+
+
+def check_preferences(user):
+    ensure_harmony_keys(user)
+    # checks = {'excludes': [], 'similarity': 50, 'groups': 3, 'possible': 0, 'recommended': {},
+    #           'rec_limit': 3, 'tastes': {}, 'ingredient_weights': json.dumps({}), 'sticky_weights': {},
+    #           'recipe_ids': {}, 'menu_weight': 1, 'algorithm': 'Balanced'}
+    # for preference in list(checks.keys()):
+    #     if preference in user.harmony_preferences:
+    #         checks[preference] = user.harmony_preferences[preference]
+    # user.harmony_preferences = checks
+    if user.extras == '' or user.extras is None:
+        user.extras = []
+    db.session.commit()
+
+# def transfer_site_changes():
+#     for recipe in Recipes.query.all():
+#         recipe.quantity = {' '.join([word.capitalize() for word in ingredient.split(' ')]): [1, 'Unit']
+#                            for ingredient in recipe.content.split(', ')}
+#         recipe.title = ' '.join([word.capitalize() for word in recipe.title.split(' ')])
+#     for user in User.query.all():
+#         user.harmony_preferences = {'excludes': [], 'similarity': 50, 'groups': 3, 'possible': 0, 'recommended': {},
+#                                     'rec_limit': 3, 'tastes': {}, 'ing_gen_weights': {}, 'ing_pair_weights': {},
+#                                     'recipe_ids': {}, 'menu_weight': 1}
+#          user.extras = []
+#     for aisle in Aisles.query.all():
+#         aisle.content = ', '.join([' '.join([word.capitalize() for word in ingredient.split(' ')])
+#                                    for ingredient in aisle.content.split(', ')])
+#         aisle.title = ' '.join([word.capitalize() for word in aisle.title.split(' ')])
+#         aisle.store = ' '.join([word.capitalize() for word in aisle.store.split(' ')])
+
+
+# for recipe in Recipes.query.all():
+#     recipe.quantity = {' '.join([word.capitalize() for word in ingredient.split(' ')]): [1, 'Unit']
+#                        for ingredient in recipe.content.split(', ')}
+#     recipe.title = ' '.join([word.capitalize() for word in recipe.title.split(' ')])
+# for user in User.query.all():
+#     user.harmony_preferences = {'excludes': [], 'similarity': 50, 'groups': 3, 'possible': 0, 'recommended': {},
+#                                 'rec_limit': 3, 'tastes': {}, 'ingredient_weights': {}, 'sticky_weights': {},
+#                                 'recipe_ids': {}, 'menu_weight': 1}
+#     user.extras = []
+# for aisle in Aisles.query.all():
+#     aisle.content = ', '.join([' '.join([word.capitalize() for word in ingredient.split(' ')])
+#                                for ingredient in aisle.content.split(', ')])
+#     aisle.title = ' '.join([word.capitalize() for word in aisle.title.split(' ')])
+#     aisle.store = ' '.join([word.capitalize() for word in aisle.store.split(' ')])
