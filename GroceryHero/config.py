35f6c4286ec02ccb2b8c49c556c3f23283cf1b6f
almaraz97 <almaraz97@ufl.edu>
2020-11-07 17:31:18 -0500

    Add server compatibility

--- GroceryHero/config.py
+++ GroceryHero/config.py
@@ -1,12 +1,12 @@
+import os
+
 
 class Config:
-    # set env variable here
-    SECRET_KEY = '5c8485000a8940797e4ee263bcfa3de6'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    #set env variable here
-    MAIL_USERNAME = 'alejandro.almaraz15@gmail.com'  # os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = 'ljtxilvndxtrrznw' #'Almaraz97'  # os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from GroceryHero import create_app
+import os
 
 
 app = create_app()
 
+server = True if os.environ.get('SERVER') == 'True' else False
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=server)
