ca6ba5b0cf0faa31dbad14388277323968ebd2b3
almaraz97 <almaraz97@ufl.edu>
2020-12-01 11:31:45 -0500

    Add models.py column for later use

--- GroceryHero/models.py
+++ GroceryHero/models.py
@@ -68,6 +68,7 @@ class Recipes(db.Model):  # Add picture of recipe in recipe page
     public = db.Column(db.Boolean, nullable=False, default=False)
     eaten = db.Column(db.Boolean, nullable=False, default=False)
     servings = db.Column(db.Integer, nullable=True, default=0)
+    # yield = db.Column(db.Integer, nullable=True, default=1)
     # downloads = db.Column(db.Integer, nullable=True, default=0)
     # other_eaten = db.Column(db.Integer, nullable=True, default=0)
 
