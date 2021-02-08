229c04070ecee5ee4da9d95fc47f00ff01c827d6
alexa <almaraz97@ufl.edu>
2021-02-08 17:50:17 -0500

    Change stats page logic to find average menu harmony after accounting for deleted recipes may be <2 per clear thus insufficient recipes to find harmony

--- GroceryHero/Main/routes.py
+++ GroceryHero/Main/routes.py
@@ -185,8 +185,8 @@ def stats():  # Bar chart of recipe frequencies, ingredient frequencies, recipe 
         harmony = round((norm_stack({r.title: r.quantity.keys() for r in all_recipes}) * 100), 5)
         avg_harmony = []
         for batch in history2:
-            if len(batch) > 1:
-                recs = {recipe.title: recipe.quantity for recipe in Recipes.query.filter(Recipes.id.in_(batch)).all()}
+            recs = {recipe.title: recipe.quantity for recipe in Recipes.query.filter(Recipes.id.in_(batch)).all()}
+            if len(recs) > 1:
                 modifier = 1 / (len(recs) + 1) if current_user.harmony_preferences['modifier'] == 'Graded' else 1.0
                 h = (norm_stack(recs) ** modifier) * 100
                 avg_harmony.append(h)
